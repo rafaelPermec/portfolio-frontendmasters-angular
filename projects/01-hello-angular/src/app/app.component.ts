0dd3587382505e72ba2ec77d42b0dbf50334221b
decobot <capy@deco.cx>
2025-01-08 15:09:17 -0300

    01: lessons complete

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,47 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1 class="cachorro">If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+
+    <span> Musics </span>
+    <ol>
+      <li>Pirate Love - New York Dolls</li>
+      <li>Lust for Life - Iggy Pop</li>
+      <li>ZigZag Wander - Frank Zappa</li>
+    </ol>
+
+    <span> Movies </span>
+    <ol>
+      <li>Blade Runner</li>
+      <li>Matrix</li>
+      <li>2001: A Space Odyssey</li>
+    </ol>
+
+    <span> Books </span>
+    <ol>
+      <li>1984 - George Orwell</li>
+      <li>Me, Robot - Isaac Asimov</li>
+      <li>Neuromancer - William Gibson</li>
+    </ol>
+
   `,
-  styles: ``,
+  styles: `
+    .cachorro {
+      color: red;
+    }
+    p {
+      color: blue;
+    }
+    ol {
+      list-style-type: upper-roman;
+    }
+
+    li {
+      color: blue;
+
+      &::marker {
+        color: red;
+      }
+    }
+  `,
 })
 export class AppComponent {}
